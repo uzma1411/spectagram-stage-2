1c8ef87126a73749c8c501ac9d4e07e153f90a40
uzma1411 <47413610+uzma1411@users.noreply.github.com>
2022-04-04 15:05:18 +0530

    Update Feed.js

--- screens/Feed.js
+++ screens/Feed.js
@@ -31,27 +31,7 @@ export default class Feed extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <SafeAreaView style={styles.droidSafeArea} />
-                <View style={styles.appTitle}>
-                    <View style={styles.appIcon}>
-                        <Image
-                            source={require("../assets/logo.png")}
-                            style={styles.iconImage}
-                        ></Image>
-                    </View>
-                    <View style={styles.appTitleTextContainer}>
-                        <Text style={styles.appTitleText}>Spectagram</Text>
-                    </View>
-                </View>
-                <View style={styles.cardContainer}>
-                    <FlatList
-                        keyExtractor={this.keyExtractor}
-                        data={posts}
-                        renderItem={this.renderItem}
-                    />
-                </View>
-            </View>
+           // Write the code here for Feed screen
         );
     }
 }
