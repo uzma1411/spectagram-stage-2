6abfb9633d55884b3236ff4efab1868d2bb6bee5
uzma1411 <47413610+uzma1411@users.noreply.github.com>
2022-04-04 14:58:55 +0530

    Update PostCard.js

--- screens/PostCard.js
+++ screens/PostCard.js
@@ -18,33 +18,7 @@ export default class PostCard extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <View style={styles.cardContainer}>
-                    <View style={styles.authorContainer}>
-                        <View style={styles.authorImageContainer}>
-                            <Image
-                                source={require("../assets/profile_img.png")}
-                                style={styles.profileImage}
-                            ></Image>
-                        </View>
-                        <View style={styles.authorNameContainer}>
-                            <Text style={styles.authorNameText}>{this.props.post.author}</Text>
-                        </View>
-                    </View>
-                    <Image source={require("../assets/post.jpeg")} style={styles.postImage} />
-                    <View style={styles.captionContainer}>
-                        <Text style={styles.captionText}>
-                            {this.props.post.caption}
-                        </Text>
-                    </View>
-                    <View style={styles.actionContainer}>
-                        <View style={styles.likeButton}>
-                            <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
-                            <Text style={styles.likeText}>12k</Text>
-                        </View>
-                    </View>
-                </View>
-            </View>
+           // Write the code for Designing the post card here
         );
     }
 }
